116bec563e67726e777024a6253ea48381d836ab
malin@google.com <malin@google.com@86435aeb-f8bb-df54-6b94-11fb64568b14>
2011-09-16 21:04:45 +0000

    Added apache config

--- trunk/ast/app/controllers/application.rb
+++ trunk/ast/app/controllers/application.rb
@@ -23,12 +23,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # :secret => '9d2eebad4bc10953ad230a2db7c61251'
 
   
-  audit DnsZone, Vip, NagiosHostGroup, NagiosService, ServiceContainer, Asset => { :except => :ocs_history }, Interface => { :parent => :asset }, DnsCnameDetail => {:parent => :dns_cname}
-  #audit Interface => { :parent => :asset } 
-  #audit DnsCnameDetail => {:parent => :dns_cname}
+#  audit DnsZone, Vip, NagiosHostGroup, NagiosService, ServiceContainer, Asset => { :except => :ocs_history }, Interface => { :parent => :asset }, DnsCnameDetail => {:parent => :dns_cname}
   
-  #raise Interface.acts_as_audited.inspect
-
   # Find nagios hostgroup
   def nagios_find_my_hostgroup(hostname,hostgroup_map = nil)
     
